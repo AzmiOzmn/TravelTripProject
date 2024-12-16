1f72f20fbcb21c7d1041f5db3d4cf31162ea730e
AzmiOzmn <azmiozmen5234@gmail.com>
2024-12-17 01:25:08 +0300

    Updated

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -15,12 +15,40 @@ namespace TravelTripProject.Controllers
         Context db = new Context();
         public ActionResult Index()
         {
-            var degerler = db.Blogs.ToList();
+            var degerler = db.Blogs.Take(4).ToList();
             return View(degerler);
         }
         public ActionResult About()
         {
             return View();
+        }
+
+        public PartialViewResult Partial1()
+        {
+            var partial1 = db.Blogs.OrderByDescending(x=>x.ID).Take(2).ToList();    
+            return PartialView(partial1);
+        }
+        public PartialViewResult Partial2()
+        {
+            var partial2 = db.Blogs.Where(x => x.ID==1).ToList();
+            return PartialView(partial2);
+        }
+        public PartialViewResult Partial3()
+        {
+            var partial3 = db.Blogs.ToList();
+            return PartialView(partial3); 
+        }
+
+        public PartialViewResult Partial4()
+        {
+            var partial4 = db.Blogs.Take(3).ToList();
+            return PartialView(partial4);
+        }
+
+        public PartialViewResult Partial5()
+        {
+            var partial5 = db.Blogs.Take(3).OrderByDescending(x=>x.ID).ToList();
+            return PartialView(partial5);
         }
     }
 }
